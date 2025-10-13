8e859e27b24d7a5bdf590ef66ab2496224d84daa
ALEXANDRU-COSMIN PASCU <alexandru3.pascu@localhost.localdomain>
2025-10-13 12:29:32 +0300

    hack detected

--- module/Application/src/Module.php
+++ module/Application/src/Module.php
@@ -4,8 +4,25 @@ declare(strict_types=1);
 
 namespace Application;
 
+use Laminas\EventManager\EventManager;
+
 class Module
 {
+    function onBootstrap($e): void
+    {
+        //var_dump("we are in");
+        $events = new EventManager();
+        $events->attach('hack_detected', function ($e) {
+            //send email
+            var_dump('Hack has been detected');
+        });
+
+
+
+        $params = []; // define parameters if needed, or adjust as necessary
+        $events->trigger('hack_detected', null);
+    }
+
     public function getConfig(): array
     {
         /** @var array $config */
